b4427cd17274e4e44219e849ecb1d468c480f0c5
aliaraa <mohammadali.ara@gmail.com>
2024-11-24 13:22:25 +0100

    Initial Commit

---
+++ Equipment Booking/Equipment_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Equipment_BookingApp.swift
+//  Equipment Booking
+//
+//  Created by Ali Ara on 2024-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Equipment_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
