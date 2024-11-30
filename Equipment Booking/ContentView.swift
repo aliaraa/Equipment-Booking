0c8bc43c0eb433519a358ed24b3fd028fdfbd9ca
aliaraa <mohammadali.ara@gmail.com>
2024-11-30 17:41:42 +0100

    Init

--- Equipment Booking/ContentView.swift
+++ Equipment Booking/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Spacer()
+                    Text("Equipment A")
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
