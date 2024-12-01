50915ee9d72a340d19ecb0926f9d4600af6096b5
aliaraa <mohammadali.ara@gmail.com>
2024-12-01 15:33:10 +0100

    Added Serach-View

--- Equipment Booking/ContentView.swift
+++ Equipment Booking/ContentView.swift
@@ -2,65 +2,14 @@
 //  ContentView.swift
 //  Equipment Booking
 //
-//  Created by Ali Ara on 2024-11-24.
+//  Created by Ali Ara on 2024-12-01.
 //
 
 import SwiftUI
 
-
-
 struct ContentView: View {
-    @State private var tools = toolData
-    @State private var searchText = ""
-    
-    var filteredTools: [Tool] {
-        if searchText.isEmpty{
-            return tools
-        } else {
-            return tools.filter{
-                $0.name.localizedCaseInsensitiveContains(searchText) ||
-                $0.description.localizedCaseInsensitiveContains(searchText)
-            }
-        }
-    }
-    
     var body: some View {
-        NavigationStack{
-            VStack{
-                TextField("Search...", text: $searchText)
-                    .padding(10)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .padding(.horizontal)
-                List(filteredTools){tool in
-                    HStack{
-                        VStack(alignment: .leading){
-                            Text(tool.name)
-                                .font(.headline)
-                            Text(tool.description)
-                                .font(.subheadline)
-                                .foregroundColor(Color.gray)
-                            Text("Price Per Day: \(tool.price)$")
-                                .foregroundColor(Color.red)
-                                .font(.caption)
-                        }
-                        Spacer()
-                        
-                        if tool.isAvailable{
-                            Text("Available")
-                                .font(.caption)
-                                .foregroundColor(Color.green)
-                        } else {
-                            Text("Unavailable")
-                                .font(.caption)
-                                .foregroundColor(Color.red)
-                        }
-                        
-                    }
-                    
-                }
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
