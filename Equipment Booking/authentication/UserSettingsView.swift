3eac566f3bc468d3dbb9b0307c65bbb03c3c0210
Rene PM <rmbanguka@gmail.com>
2024-12-08 22:43:58 +0100

    minor settings

---
+++ Equipment Booking/authentication/UserSettingsView.swift
@@ -0,0 +1,52 @@
+//
+//  UserSettingsView.swift
+//  Equipment Booking
+//
+//  Created by Rene Mbanguka on 12/9/24.
+//
+
+import SwiftUI
+
+@MainActor
+
+final class UserSettingsViewModel: ObservableObject{
+    
+    
+    func signOut ()  throws {
+        try authenticationManager.shared.singOut()
+    }
+    
+}
+
+struct UserSettingsView: View {
+    
+    @StateObject private var viewModel = UserSettingsViewModel()
+    @Binding var showSignInView: Bool
+    
+    var body: some View {
+        List{
+            Button("Log out"){
+                Task {
+                    do {
+                        try viewModel.signOut()
+                        showSignInView = true
+                    }
+                    catch{
+                        print(error)
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
+    }
+}
+
+struct UserSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        NavigationStack{
+            UserSettingsView (showSignInView: .constant(false))
+        }
+    }
+}
