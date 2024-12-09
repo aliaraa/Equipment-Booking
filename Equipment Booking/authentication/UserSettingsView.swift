9ca8de5d1b22a134b9a0c0b349391ae5a4d79bb6
Rene PM <rmbanguka@gmail.com>
2024-12-09 16:01:51 +0100

    Authentication management functions

---
+++ Equipment Booking/authentication/UserSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettingsView.swift
+//  Equipment Booking
+//
+//  Created by Rene Mbanguka on 12/9/24.
+//
+
+import SwiftUI
+
+struct UserSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSettingsView()
+}
