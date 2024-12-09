95ccfd3722838dd6ff5f02bdb566d808117188bf
Rene PM <rmbanguka@gmail.com>
2024-12-09 01:11:36 +0100

    Create authentication manager file

---
+++ Equipment Booking/authentication/AuthenticationManagement.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationManagement.swift
+//  Equipment Booking
+//
+//  Created by Rene Mbanguka on 12/9/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+
+final class authenticationManager {
+    static let shared  = authenticationManager()
+    private init () {
+        
+    }
+}
