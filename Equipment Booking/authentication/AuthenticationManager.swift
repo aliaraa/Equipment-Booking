4160435e19849d2225d85741851df8e061451f4d
Rene PM <rmbanguka@gmail.com>
2024-12-17 03:51:31 +0100

    Adding function to fetch user details from firebase

--- Equipment Booking/authentication/AuthenticationManager.swift
+++ Equipment Booking/authentication/AuthenticationManager.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+import Firebase
 import FirebaseAuth
+// import FirebaseFirestore
 
 struct AuthDataResultModel {
     let uid: String
@@ -24,6 +26,8 @@ struct AuthDataResultModel {
 
 final class AuthenticationManager {
     static let shared  = AuthenticationManager()
+    @State private var firstname: String = "Loading..." // Default placeholder for user name
+    private let db = Firestore.firestore()
     private init () {}
     
     // Function to get user
@@ -43,7 +47,7 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: authDataResult.user)
     }
     
-    @discardableResult 
+    @discardableResult
     func signInUser (email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
         return AuthDataResultModel(user: authDataResult.user)
@@ -77,6 +81,38 @@ final class AuthenticationManager {
         try Auth.auth().signOut()
         
     }
+    
+    // Function to fetch user details from firestore db
+    func fetchUserNames(){
+        guard let user = Auth.auth().currentUser else {
+            print("No user currently signed in")
+            return
+        }
+        let userUID = user.uid // get the signed in user's UID from authentication
+        let userDocRef = db.collection("users").document(userUID) //get corresponding user details from firebase users collection
+        
+        userDocRef.getDocument{ document, error in
+            if let error = error {
+                print ("Error fetching user details: \(error.localizedDescription)")
+                self.firstName = "Unkown " //if no use info found
+                return
+            }
+            
+            if let document = document, document.exists {
+                if let fetchedFirstname = document.data()?["firsname"] as String {
+                    self.firstname = fetchedFirstname
+                }
+                else{
+                    self.firstname = "No Name"
+                }
+            } else {
+                print("Document does not exist for user \(userUID).")
+                self.firstname = "Unkown"
+                
+            }
+        }
+    }
+        
     
     
 }
