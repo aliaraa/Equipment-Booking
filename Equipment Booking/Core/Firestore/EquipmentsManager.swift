38c7677768680717f6a8ddbb8ad6765083a7a729
Rene PM <rmbanguka@gmail.com>
2025-01-06 05:35:37 +0100

    Added an EquipmentManager class to manage equipment collection

--- Equipment Booking/Core/Firestore/EquipmentsManager.swift
+++ Equipment Booking/Core/Firestore/EquipmentsManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreCombineSwift
+
+final class EquipmentManager {
+    
+    static let shared = EquipmentManager()
+    private init() {}
+    
+    private let equipmentsCollection = Firestore.firestore().collection("equipments")
+    
+    //function to get document/equipment for a given equipmentId
+    private func equipmentDocument(equipmentId: String) -> DocumentReference {
+        equipmentsCollection.document(equipmentId)
+    }
+    
+    func uploadEquipment(equipment: Equipment) async throws {
+        try equipmentDocument(equipmentId: equipment.equip_id).setData(from: equipment, merge: false)
+    }
+    
+    func getAllEquipments() async throws -> [Equipment] {
+        let snapshot = try await equipmentsCollection.getDocuments()
+        
+        var equipments: [Equipment] = []
+        for document in snapshot.documents {
+            let equipment = try document.data(as: Equipment.self)
+            equipments.append(equipment)
+        }
+        
+        return equipments
+        
+    }
+    
+}
