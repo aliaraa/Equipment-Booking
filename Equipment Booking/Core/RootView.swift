b634874649ce568bbb1b10900fb8ac6622ccd1c7
Rene PM <rmbanguka@gmail.com>
2025-01-06 05:38:34 +0100

    structure for equipment data

--- Equipment Booking/Core/RootView.swift
+++ Equipment Booking/Core/RootView.swift
@@ -16,6 +16,7 @@ struct RootView: View {
         ZStack{
             if !showSignInView{
                 NavigationStack {
+                    //EquipmentListingView()
                     ProfileView(showSignInView: $showSignInView)
                     //UserSettingsView(showSignInView: $showSignInView)
                 }
--- Equipment Booking/Utilities/EquipmentsDatabase.swift
+++ Equipment Booking/Utilities/EquipmentsDatabase.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct EquipmentArray: Codable {
+    let equipments: [Equipment]
+    let total, skip, limit: Int
+}
+
+struct Equipment: Identifiable, Codable {
+    let equip_id: String // unique id
+    var id: String {equip_id} // conform to Identifiable by mapping 'id' to 'equip_id'
+    let availability_status: String?
+    let description: String?
+    let equipment_main_category: String?
+    let equipment_sub_category: String?
+    let equipment_name: String?
+    let img_name: String?
+    let img_url: String?
+    let manufacturer: String?
+    let rental_price_per_day: Double
+    
+}
+
+final class EquipmentsDatabase {
+    
+    static let equipments: [Equipment] = [
+        Equipment(equip_id: "CC-C-2", availability_status: "available", description: "The Liebherr 32 TTR is the crane for you who value maximum flexibility and mobility. This crawler construction crane with self-elevating properties expands your possibilities to get ahead on the jobsite.", equipment_main_category: "Construction Cranes", equipment_sub_category: "Crawler", equipment_name: "Liebherr 32 TTR", img_name: "712322-31.Jpg", img_url: "https://storage.googleapis.com/equipment-management-db.firebasestorage.app/Equipment_imgs/712322-31.Jpg", manufacturer: "Liebherr", rental_price_per_day: 100),
+        
+    ]
+}
